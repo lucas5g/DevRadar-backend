4c7e3166f931bc8a6a1b3f407bf6cb1a0d8ef7a8
lucasg5 <lucasdesousa19@hotmail.com>
2020-01-26 20:37:00 -0300

    websocket error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const http = require('http')
 const routes = require('./routes')
 
+const { setupWebsocket } = require('./websocket')
+
 const app = express()
+const server = http.Server(app)
+
+setupWebsocket(server)
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-m8pau.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -16,4 +22,4 @@ app.use(express.json())
 app.use(routes)
 
 
-app.listen(8000)
+server.listen(8000)
